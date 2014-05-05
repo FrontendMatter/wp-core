fc6f29e7033f1e3b3f91ee13d8ba612822fe8966
Laza Bogdan <laza.bogdan@macro-net.ro>
2014-05-05 07:32:00 +0300

    Added abstract Shortcode library;

---
+++ src/Mosaicpro/WpCore/Shortcode.php
@@ -0,0 +1,48 @@
+<?php namespace Mosaicpro\WpCore;
+
+/**
+ * Class Shortcode
+ * @package Mosaicpro\WpCore
+ */
+abstract class Shortcode
+{
+    /**
+     * Holds a Shortcode instance
+     * @var
+     */
+    protected static $instance;
+
+    /**
+     * Constructor
+     */
+    public function __construct(){}
+
+    /**
+     * Get a Shortcode Singleton instance
+     * @return static
+     */
+    public static function getInstance()
+    {
+        $class = get_called_class();
+        if (is_null(self::$instance[$class]))
+        {
+            self::$instance[$class] = new $class;
+        }
+        return self::$instance[$class];
+    }
+
+    /**
+     * Initialize the Shortcode
+     */
+    public static function init()
+    {
+        $instance = forward_static_call([get_called_class(), 'getInstance']);
+        $instance->addShortcode();
+        return $instance;
+    }
+
+    /**
+     * Add the Shortcode to WP
+     */
+    abstract function addShortcode();
+}
